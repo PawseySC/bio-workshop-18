4771763d9e33371dbe3525f44ca9ac364111375b
Marco De La Pierre <marco.delapierre@gmail.com>
2018-08-31 08:51:34 +0000

    mod to HPC run fastqc

--- step3_porting_to_HPC/run-fastqc-inputfile-shifter.sh
+++ step3_porting_to_HPC/run-fastqc-inputfile-shifter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH --account=pawsey0001
+#SBATCH --account=<your-pawsey-project>
 #SBATCH --partition=workq
 #SBATCH --ntasks=1
 #SBATCH --time=00:05:00
@@ -14,4 +14,4 @@ fastqc_cont=quay.io/biocontainers/fastqc:0.11.7--4
 
 
 # /group and /scratch are visible on Pawsey HPC systems
-$srun_cmd shifter run $fastqc_cont fastqc SRR6166481_10k_1.fastq
+$srun_cmd shifter run $fastqc_cont fastqc -o ../../data_files/ ../../data_files/SRR6166481_10k_1.fastq
