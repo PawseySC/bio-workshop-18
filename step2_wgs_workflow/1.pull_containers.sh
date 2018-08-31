f4e696c9d15b952be6d6a6b70ca0ba97673d6997
Marco De La Pierre <marco.delapierre@gmail.com>
2018-08-31 09:18:31 +0000

    more on step2

---
+++ step2_wgs_workflow/1.pull_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+  
+# this is not required for pulling
+# docker login -u 'user' -p 'password' quay.io
+
+
+# hint: you need to execute multiple docker pull commands
+# refer to the file named 'containers' for the list of required containers
