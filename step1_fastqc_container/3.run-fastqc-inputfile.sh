25c10b35c9b0685d96dd1031df70eb5e32f00e0f
Marco De La Pierre <marco.delapierre@gmail.com>
2018-08-31 13:31:26 +0000

    compressed inputs fastq.gz

--- step1_fastqc_container/3.run-fastqc-inputfile.sh
+++ step1_fastqc_container/3.run-fastqc-inputfile.sh
@@ -3,6 +3,6 @@
 fastqc_cont=quay.io/biocontainers/fastqc:0.11.7--4
 run_flags="--rm -v $(pwd):/data -w /data"
 
-cp -p ../data_files/SRR6166481_10k_?.fastq .
+cp -p ../data_files/SRR6166481_10k_?.fastq.gz .
 
-docker run $run_flags $fastqc_cont fastqc SRR6166481_10k_1.fastq
+docker run $run_flags $fastqc_cont fastqc SRR6166481_10k_1.fastq.gz
