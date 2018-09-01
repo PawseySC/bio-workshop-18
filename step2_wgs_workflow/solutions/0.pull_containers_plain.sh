8d50e7e8fdd0ad0081cfc0ffabd537b76fe72207
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-01 13:36:59 +0000

    removed bedtools from workflow step2

--- step2_wgs_workflow/solutions/0.pull_containers_plain.sh
+++ step2_wgs_workflow/solutions/0.pull_containers_plain.sh
@@ -8,7 +8,6 @@ docker pull quay.io/biocontainers/fastqc:0.11.7--4
 docker pull quay.io/biocontainers/trimmomatic:0.38--0
 docker pull quay.io/biocontainers/bowtie2:2.3.4.2--py27h2d50403_0
 docker pull quay.io/biocontainers/samtools:1.9--h46bd0b3_0
-docker pull quay.io/biocontainers/bedtools:2.23.0--he941832_1
 docker pull quay.io/biocontainers/velvet:1.2.10--1
 docker pull biocontainers/mummer:v3.23dfsg-2-deb_cv1
 docker pull quay.io/biocontainers/bcftools:1.9--h4da6232_0
