60d793b48c47b28f078d91fc6d71a85e0aa97666
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-01 15:21:45 +0000

    minor mods

--- step2_wgs_workflow/solutions/1.fastqc.sh
+++ step2_wgs_workflow/solutions/1.fastqc.sh
@@ -6,4 +6,5 @@ run_flags="--rm -v $(pwd):/data -w /data"
 cp -p ../data_files/SRR6166481_sub_[12].fastq.gz .
 
 docker run $run_flags $fastqc_cont fastqc -o . SRR6166481_sub_1.fastq.gz
+
 docker run $run_flags $fastqc_cont fastqc -o . SRR6166481_sub_2.fastq.gz
