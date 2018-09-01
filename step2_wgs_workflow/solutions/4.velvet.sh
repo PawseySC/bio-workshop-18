5165270d12f2a249165da4512dd083553c597058
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-01 15:01:47 +0000

    mods to step2 4.velvet

--- step2_wgs_workflow/solutions/4.velvet.sh
+++ step2_wgs_workflow/solutions/4.velvet.sh
@@ -3,8 +3,8 @@
 velvet_cont=quay.io/biocontainers/velvet:1.2.10--1
 run_flags="--rm -v $(pwd):/data -w /data"
 
-#docker run $run_flags $velvet_cont velveth velvet 29 -shortPaired -fastq.gz -separate SRR6166481_10k_hitting.fastq.1.gz SRR6166481_10k_hitting.fastq.2.gz
-docker run $run_flags $velvet_cont velveth velvet 29 -shortPaired -fastq.gz -separate SRR6166481_10k_1_paired.fastq.gz SRR6166481_10k_2_paired.fastq.gz
+#docker run $run_flags $velvet_cont velveth out_velvet 29 -shortPaired -fastq.gz -separate SRR6166481_10k_hitting.fastq.1.gz SRR6166481_10k_hitting.fastq.2.gz
+docker run $run_flags $velvet_cont velveth out_velvet 29 -shortPaired -fastq.gz -separate SRR6166481_10k_1_paired.fastq.gz SRR6166481_10k_2_paired.fastq.gz
 echo ASPETTA PIPPO ASPETTA
-docker run $run_flags $velvet_cont velvetg velvet -exp_cov 100 -min_contig_lgth 2000 -ins_length 300 -cov_cutoff 50 -clean yes
-cp -p velvet/contigs.fa .
+docker run $run_flags $velvet_cont velvetg out_velvet -exp_cov 100 -min_contig_lgth 2000 -ins_length 300 -cov_cutoff 50 -clean yes
+cp -p out_velvet/contigs.fa SRR6166481_10k_contigs.fa
