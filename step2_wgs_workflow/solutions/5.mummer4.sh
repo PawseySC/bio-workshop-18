7f0b4896c42c797fba0a39044ed4e9f729388135
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-02 13:19:49 +0000

    step2 mummer4 - added gnuplot command

--- step2_wgs_workflow/solutions/5.mummer4.sh
+++ step2_wgs_workflow/solutions/5.mummer4.sh
@@ -6,4 +6,4 @@ run_flags="--rm -v $(pwd):/data -w /data"
 docker run $run_flags $mummer4_cont mummer -b Fagopyrum_esculentum.fasta SRR6166481_sub_contigs.fa >SRR6166481_sub_mummer.out
 docker run $run_flags $mummer4_cont mummerplot -png -p SRR6166481_sub_mummer SRR6166481_sub_mummer.out
 
-
+gnuplot SRR6166481_sub_mummer.gp
