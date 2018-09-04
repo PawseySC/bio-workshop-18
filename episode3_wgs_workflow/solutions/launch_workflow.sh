0a1705ba089416e5bd017e6bbb5e0c4dd8fab282
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-04 22:06:54 +0800

    mods to launch wf scripts episode 3

--- episode3_wgs_workflow/solutions/launch_workflow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash 0.pull_containers_for_loop.sh && \
-bash 1.fastqc.sh && \
-bash 2.trimmomatic.sh && \
-bash 3.bowtie2.sam.sh && \
-bash 4.velvet.sh && \
-bash 5.mummer4.sh && \
-bash 6.bcftools.sam.sh 
