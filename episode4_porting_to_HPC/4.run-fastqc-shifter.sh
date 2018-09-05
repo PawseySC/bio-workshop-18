f8b9d86f51a5e03eba4fb3a7bf84f5b2b83d5793
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-05 10:22:50 +0800

    mods to ex in ep.4

--- episode4_porting_to_HPC/4.run-fastqc-shifter.sh
+++ episode4_porting_to_HPC/4.run-fastqc-shifter.sh
@@ -8,4 +8,4 @@ fastqc_cont=quay.io/biocontainers/fastqc:0.11.7--4
 cp -p ../data_files/SRR6166481_sub_1.fastq.gz .
 
 # /group and /scratch are visible on Pawsey HPC systems
-shifter run $fastqc_cont fastqc -o . SRR6166481_sub_1.fastq.gz
+shifter run $fastqc_cont fastqc SRR6166481_sub_1.fastq.gz
--- episode4_porting_to_HPC/5.run-fastqc-shifter-slurm.sh
+++ episode4_porting_to_HPC/5.run-fastqc-shifter-slurm.sh
@@ -15,4 +15,4 @@ fastqc_cont=quay.io/biocontainers/fastqc:0.11.7--4
 cp -p ../data_files/SRR6166481_sub_1.fastq.gz .
 
 # /group and /scratch are visible on Pawsey HPC systems
-$srun_cmd shifter run $fastqc_cont fastqc -o . SRR6166481_sub_1.fastq.gz
+$srun_cmd shifter run $fastqc_cont fastqc SRR6166481_sub_1.fastq.gz
