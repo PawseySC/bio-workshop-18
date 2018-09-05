bd78c450356ecbbe34913ca215f76e87688c9f6f
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-05 10:17:22 +0800

    new exercise for ep.4

---
+++ episode4_porting_to_HPC/4.run-fastqc-shifter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+module load shifter
+
+
+fastqc_cont=quay.io/biocontainers/fastqc:0.11.7--4
+
+cp -p ../data_files/SRR6166481_sub_?.fastq.gz .
+
+# /group and /scratch are visible on Pawsey HPC systems
+shifter run $fastqc_cont fastqc -o . SRR6166481_sub_1.fastq.gz
