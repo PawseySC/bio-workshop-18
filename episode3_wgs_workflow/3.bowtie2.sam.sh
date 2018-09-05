49125127c8b5bfbe047bd81636716917bbefeed4
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-05 20:41:05 +0800

    mods to scripts and pic for ep.3

--- episode3_wgs_workflow/3.bowtie2.sam.sh
+++ episode3_wgs_workflow/3.bowtie2.sam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # note: we are defining two distinc container variables for usage in this script
-bowtie2_cont=quay.io/biocontainers/bowtie2:2.3.4.2--py27h2d50403_0
-samtools_cont=quay.io/biocontainers/samtools:1.9--h46bd0b3_0
-run_flags="--rm -v $(pwd):/data -w /data"
+bowtie2_cont=
+samtools_cont=
+run_flags=
 
 cp -p ../data_files/Fagopyrum_esculentum.fasta .
 
