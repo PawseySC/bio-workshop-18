86d6af55cca684581d847da8da403dbe931104aa
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-08 22:34:32 +0800

    mod to script fastqc ep.3

--- episode3_wgs_workflow/1.fastqc.sh
+++ episode3_wgs_workflow/1.fastqc.sh
@@ -3,6 +3,6 @@ run_flags="--rm -v $(pwd):/data -w /data"
 
 cp -p ../data_files/SRR6166481_sub_[12].fastq.gz .
 
-fastqc -o . SRR6166481_sub_1.fastq.gz
+docker run $run_flags quay.io/biocontainers/fastqc:0.11.7--4 fastqc -o . SRR6166481_sub_1.fastq.gz
 
-fastqc -o . SRR6166481_sub_2.fastq.gz
+docker run $run_flags quay.io/biocontainers/fastqc:0.11.7--4 fastqc -o . SRR6166481_sub_2.fastq.gz
