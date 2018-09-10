c4e657bc1ee3bdc6ca16d62af4a264724bb4be97
Marco De La Pierre <marco.delapierre@gmail.com>
2018-09-10 14:49:21 +0800

    mods to tutorial ep.3 - pull, eps.1,2

--- episode3_wgs_workflow/2.trimmomatic.sh
+++ episode3_wgs_workflow/2.trimmomatic.sh
@@ -2,7 +2,7 @@
 run_flags="--rm -v $(pwd):/data -w /data"
 
 java -jar <path-to-trimmomatic-jar>/trimmomatic.jar \
-	PE SRR6166481_sub_1.fastq.gz SRR6166481_sub_2.fastq.gz \
-	SRR6166481_sub_1_paired.fastq.gz SRR6166481_sub_1_unpaired.fastq.gz \
-	SRR6166481_sub_2_paired.fastq.gz SRR6166481_sub_2_unpaired.fastq.gz \
-	ILLUMINACLIP:adapters/all_PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    PE SRR6166481_sub_1.fastq.gz SRR6166481_sub_2.fastq.gz \
+    SRR6166481_sub_1_paired.fastq.gz SRR6166481_sub_1_unpaired.fastq.gz \
+    SRR6166481_sub_2_paired.fastq.gz SRR6166481_sub_2_unpaired.fastq.gz \
+    ILLUMINACLIP:adapters/all_PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
--- episode3_wgs_workflow/workflow_start_here.sh
+++ episode3_wgs_workflow/workflow_start_here.sh
@@ -9,10 +9,10 @@ fastqc -o . SRR6166481_sub_2.fastq.gz
 
 # 2. Cleaning - Trimmomatic
 java -jar <path-to-trimmomatic-jar>/trimmomatic.jar \
-	PE SRR6166481_sub_1.fastq.gz SRR6166481_sub_2.fastq.gz \
-	SRR6166481_sub_1_paired.fastq.gz SRR6166481_sub_1_unpaired.fastq.gz \
-	SRR6166481_sub_2_paired.fastq.gz SRR6166481_sub_2_unpaired.fastq.gz \
-	ILLUMINACLIP:adapters/all_PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    PE SRR6166481_sub_1.fastq.gz SRR6166481_sub_2.fastq.gz \
+    SRR6166481_sub_1_paired.fastq.gz SRR6166481_sub_1_unpaired.fastq.gz \
+    SRR6166481_sub_2_paired.fastq.gz SRR6166481_sub_2_unpaired.fastq.gz \
+    ILLUMINACLIP:adapters/all_PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 
 # 3. Read alignment against reference - Bowtie2
